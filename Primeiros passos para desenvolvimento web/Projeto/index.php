36efaebaa711baaac89d6a90f003723f3bd14952
unknown <neto.estanislau@gmail.com>
2022-01-15 10:40:43 -0300

    Adicionando novo projeto de estudo

---
+++ Primeiros passos para desenvolvimento web/Projeto/index.php
@@ -0,0 +1,17 @@
+<html>
+
+	<head>
+		<title>MEU PRIMEIRO SITE EM PHP</title>
+	</head>
+	<body>
+		
+		<?php
+			for ($i = 0 ; $i < 10 ; $i++){
+				print("linha numero " .$i . "<br />");
+			}
+ 		?>
+		
+	</body>
+
+
+</html>
